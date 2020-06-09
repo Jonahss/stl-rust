18fa52c9a02f8c4beca86edef735813b8471024d
Jonah Stiennon <jonahss@gmail.com>
2020-06-08 19:10:54 -0700

    benchmark added

--- benches/bench.rs
+++ benches/bench.rs
@@ -6,11 +6,6 @@ use test::Bencher;
 const TEST_ASSET_PATH: &str = "./testAssets";
 const FILE_NAME: &str = "stacked_cones_slices_2.stl";
 
-#[test]
-fn parse_and_load() {
-  let solid = stl_rust::load(&format!("{}/{}", TEST_ASSET_PATH, FILE_NAME));
-  assert_eq!(solid.triangles.len(), 4400)
-}
 
 #[bench]
 fn benchmark_parse_and_load(b: &mut Bencher) {
