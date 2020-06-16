0c78fe802db4e07ef774d834bd6db5abbd620141
Jonah Stiennon <jonahss@gmail.com>
2020-06-15 19:58:10 -0700

    try to play with getting references to hashset keys

--- src/mesh.rs
+++ src/mesh.rs
@@ -5,12 +5,12 @@ use crate::stl::datatypes::{Vertex};
 
 // While Solid stores a surface as triangles, the way an STL file does, Mesh stores a list of vertices and edges.
 // Let's try storing them in an adjacency list
-pub struct Mesh {
+pub struct Mesh<'a> {
   vertices: HashMap<Vertex, usize>,
-  adjacency_list: HashMap<usize, HashSet<usize>>,
+  adjacency_list: HashMap<usize, HashSet<&'a Vertex>>,
 }
 
-impl Mesh {
+impl<'a> Mesh<'a> {
   fn add_triangle(& mut self, a: Vertex, b: Vertex, c: Vertex) {
     let a_id_option = self.vertices.get(&a);
     let a_id;
@@ -49,21 +49,21 @@ impl Mesh {
     }
     
     let a_neighbors = self.adjacency_list.entry(a_id).or_insert_with(HashSet::new);
-    a_neighbors.insert(b_id);
-    a_neighbors.insert(c_id);
+    //a_neighbors.insert(b_id);
+    //a_neighbors.insert(c_id);
 
     let b_neighbors = self.adjacency_list.entry(b_id).or_insert_with(HashSet::new);
-    b_neighbors.insert(a_id);
-    b_neighbors.insert(c_id);
+    //b_neighbors.insert(a_id);
+    //b_neighbors.insert(c_id);
 
     let c_neighbors = self.adjacency_list.entry(c_id).or_insert_with(HashSet::new);
-    c_neighbors.insert(a_id);
-    c_neighbors.insert(b_id);
+    //c_neighbors.insert(a_id);
+    //c_neighbors.insert(b_id);
   }
 }
 
-impl Mesh {
-  pub fn from_solid(solid: Solid) -> Mesh {
+impl<'a> Mesh<'a> {
+  pub fn from_solid(solid: Solid) -> Mesh<'a> {
     let mut mesh = Mesh {
       vertices: HashMap::new(),
       adjacency_list: HashMap::new(),
@@ -77,7 +77,7 @@ impl Mesh {
   }
 }
 
-impl fmt::Display for Mesh {
+impl fmt::Display for Mesh<'_> {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
     let num_edges = self.adjacency_list.values().fold(0, |acc, x| acc + x.len());
     let first = self.vertices.keys().next().expect("no vertices in mesh!");
