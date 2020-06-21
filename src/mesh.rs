bee94ba4fef8e3fe12c314406224d1f95e630374
Jonah Stiennon <jonahss@gmail.com>
2020-06-21 16:08:38 -0700

    calculating the genus works! even with multiple components. next step is to be able to segment a single stl into multiple manifolds.

--- src/mesh.rs
+++ src/mesh.rs
@@ -10,6 +10,7 @@ pub struct Mesh {
   vertices: HashMap<Vertex, VertexId>,
   vertex_lookup: HashMap<VertexId, Vertex>,
   adjacency_list: HashMap<VertexId, HashSet<VertexId>>,
+  num_triangles: i32,
 }
 
 impl Mesh {
@@ -67,6 +68,8 @@ impl Mesh {
     let c_neighbors = self.adjacency_list.entry(c_id).or_insert_with(HashSet::new);
     c_neighbors.insert(a_id);
     c_neighbors.insert(b_id);
+
+    self.num_triangles += 1;
   }
 }
 
@@ -76,6 +79,7 @@ impl Mesh {
       vertices: HashMap::new(),
       vertex_lookup: HashMap::new(),
       adjacency_list: HashMap::new(),
+      num_triangles: 0,
     };
 
     for v in solid.triangles {
@@ -84,16 +88,34 @@ impl Mesh {
 
     mesh
   }
+
+  pub fn num_edges(&self) -> i32 {
+    (self.adjacency_list.values().fold(0, |acc, x| acc + x.len()) / 2) as i32 // each edge is listed twice, once for each starting vertex
+  }
+
+  pub fn num_vertices(&self) -> i32 {
+    self.vertices.len() as i32
+  }
+
+  pub fn average_vertex_valence(&self) -> i32 {
+    self.adjacency_list.values().map(|edges| edges.len()).sum::<usize>() as i32 / self.num_vertices()
+  }
 }
 
 impl fmt::Display for Mesh {
   fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-    let num_edges = self.adjacency_list.values().fold(0, |acc, x| acc + x.len());
+    let num_edges = self.num_edges();
+    let num_vertices = self.num_vertices();
     let first = self.vertices.keys().next().expect("no vertices in mesh!");
     let vertex_display = format!("{:?}", first);
     let first_edges = self.adjacency_list.get(&0).expect("no edges");
     let first_edges: Vec<&Vertex> = first_edges.iter().map(|id| self.vertex_lookup.get(id).unwrap()).collect();
 
-    write!(f, "Mesh with {} vertices, {} edges\na vertex: {:?}, {:?}", self.vertices.len(), num_edges, vertex_display, first_edges)
+    writeln!(f, "Mesh with {} vertices, {} edges, {} triangles", num_vertices, num_edges, self.num_triangles);
+    writeln!(f, "F ≈ 2V ({} ≈ {})", self.num_triangles, 2 * num_vertices);
+    writeln!(f, "E ≈ 3V ({} ≈ {})", self.num_edges(), 3 * num_vertices);
+    writeln!(f, "Average Vertex Valence ≈ 6 ({} ≈ 6)", self.average_vertex_valence());
+    writeln!(f, "Euler formula: V - E + F = 2(1-g)  (g = {})", 1 - ((num_vertices - num_edges + self.num_triangles) / 2));
+    writeln!(f, "a vertex: {:?}, {:?}", vertex_display, first_edges)
   }
 }
